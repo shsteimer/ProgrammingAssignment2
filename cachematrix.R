a0a1eab0f91c4879e695bf889fafa18da6db3c05
Sean Steimer <ssteimer@crownpartners.com>
2014-10-21 21:03:15 -0500

    initial commit based on sample code for cache mean of vector.

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	inverse <- NULL
+	set <- function(y) {
+		x <<- y
+		inverse <<- NULL
+	}
+	get <- function {
+		x
+	}
+	getInverse <- function(inv) {
+		inverse <<- inv
+	}
+	setInverse <- function() {
+		inverse
+	}
+	list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 
 }
 
@@ -11,5 +28,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inverse <- x$getInverse()
+	if(!is.null(inverse)) {
+		message("getting cached data")
+		return(inverse)
+	}
+	
+	matrix <- x$get()
+	inverse <- solve(matrix)
+	x$setInverse(inverse)
+	inverse
 }
